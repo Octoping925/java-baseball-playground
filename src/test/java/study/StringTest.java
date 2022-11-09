f71942c5f824272b2b487f62b6c8fa80691f87b4
Octoping925 <myc1365@naver.com>
2022-11-10 00:36:37 +0900

    숫자야구게임-단위테스트 - 학습테스트 실습
    ParameterizedTest를 쓰면 테스트에 매개변수를 넣어줄 수 있다.
    valueSource에 ints, strings 등의 값을 넣어서 사용할 수 있다.
    CsvSource를 이용해서 여러 매개변수를 넣어줄 수 있다

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,43 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] splitResult = "1,2".split(",");
+        assertThat(splitResult).contains("1", "2");
+
+        String[] splitResult2 = "1".split(",");
+        assertThat(splitResult2).containsExactly("1");
+    }
+
+    String substring() {
+        return "(1,2)".substring(1, 4);
+    }
+
+    @Test
+    void charAt() {
+        char c = "abc".charAt(0);
+        assertThat(c).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("charAt으로 특정 위치의 문자를 가져올 때 위치 값을 벗어나면 StringIndexOutOfBoundsException이 발생")
+    void charAt_FAIL() {
+        assertThatThrownBy(() -> {
+           "abc".charAt(99);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+        .hasMessageContaining("String index out of range: 99");
     }
 }
